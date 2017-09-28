97b6fbd599912302c71d75500e1a479f8bae4e6b
AJ Rogers <aj.rogers@spectregames.com>
2017-09-27 20:21:47 -0400

    web.core

---
+++ poolprobase.Web.Core/poolprobaseWebCoreModule.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using Abp.AspNetCore;
+using Abp.AspNetCore.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Abp.Zero.Configuration;
+using poolprobase.Authentication.JwtBearer;
+using poolprobase.Configuration;
+using poolprobase.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+#if FEATURE_SIGNALR
+using Abp.Web.SignalR;
+#endif
+
+namespace poolprobase
+{
+    [DependsOn(
+         typeof(poolprobaseApplicationModule),
+         typeof(poolprobaseEntityFrameworkModule),
+         typeof(AbpAspNetCoreModule)
+#if FEATURE_SIGNALR 
+        ,typeof(AbpWebSignalRModule)
+#endif
+     )]
+    public class poolprobaseWebCoreModule : AbpModule
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public poolprobaseWebCoreModule(IHostingEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void PreInitialize()
+        {
+            Configuration.DefaultNameOrConnectionString = _appConfiguration.GetConnectionString(
+                poolprobaseConsts.ConnectionStringName
+            );
+
+            //Use database for language management
+            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(poolprobaseApplicationModule).GetAssembly()
+                 );
+
+            ConfigureTokenAuth();
+        }
+
+        private void ConfigureTokenAuth()
+        {
+            IocManager.Register<TokenAuthConfiguration>();
+            var tokenAuthConfig = IocManager.Resolve<TokenAuthConfiguration>();
+
+            tokenAuthConfig.SecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appConfiguration["Authentication:JwtBearer:SecurityKey"]));
+            tokenAuthConfig.Issuer = _appConfiguration["Authentication:JwtBearer:Issuer"];
+            tokenAuthConfig.Audience = _appConfiguration["Authentication:JwtBearer:Audience"];
+            tokenAuthConfig.SigningCredentials = new SigningCredentials(tokenAuthConfig.SecurityKey, SecurityAlgorithms.HmacSha256);
+            tokenAuthConfig.Expiration = TimeSpan.FromDays(1);
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(poolprobaseWebCoreModule).GetAssembly());
+        }
+    }
+}
