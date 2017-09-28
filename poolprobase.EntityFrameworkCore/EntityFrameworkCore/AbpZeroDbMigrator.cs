9ce0622f694161227ffec4dcd03441fb6d0805e4
AJ Rogers <aj.rogers@spectregames.com>
2017-09-27 20:12:44 -0400

    Entity

---
+++ poolprobase.EntityFrameworkCore/EntityFrameworkCore/AbpZeroDbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Domain.Uow;
+using Abp.EntityFrameworkCore;
+using Abp.MultiTenancy;
+using Abp.Zero.EntityFrameworkCore;
+
+namespace poolprobase.EntityFrameworkCore
+{
+    public class AbpZeroDbMigrator : AbpZeroDbMigrator<poolprobaseDbContext>
+    {
+        public AbpZeroDbMigrator(
+            IUnitOfWorkManager unitOfWorkManager,
+            IDbPerTenantConnectionStringResolver connectionStringResolver,
+            IDbContextResolver dbContextResolver) :
+            base(
+                unitOfWorkManager,
+                connectionStringResolver,
+                dbContextResolver)
+        {
+
+        }
+    }
+}
