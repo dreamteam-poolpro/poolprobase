8a55c8dee8c2af87f38d67117eed3c34e39a44b7
AJ Rogers <aj.rogers@spectregames.com>
2017-09-27 20:01:59 -0400

    Application folder

---
+++ poolprobase.Application/Authorization/Accounts/Dto/TenantAvailabilityState.cs
@@ -0,0 +1,9 @@
+﻿namespace poolprobase.Authorization.Accounts.Dto
+{
+    public enum TenantAvailabilityState
+    {
+        Available = 1,
+        InActive,
+        NotFound
+    }
+}
---
+++ poolprobase.Application/Configuration/ConfigurationAppService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Abp.Authorization;
+using Abp.Runtime.Session;
+using poolprobase.Configuration.Dto;
+
+namespace poolprobase.Configuration
+{
+    [AbpAuthorize]
+    public class ConfigurationAppService : poolprobaseAppServiceBase, IConfigurationAppService
+    {
+        public async Task ChangeUiTheme(ChangeUiThemeInput input)
+        {
+            await SettingManager.ChangeSettingForUserAsync(AbpSession.ToUserIdentifier(), AppSettingNames.UiTheme, input.Theme);
+        }
+    }
+}
