297ce6e421e668441dd346248a48ecd27c3035cb
Joseph Wilson <josephawilson86@gmail.com>
2017-10-20 01:37:35 -0700

    clearing things out so i can push the current version

--- aspnet-core/src/poolprobase.Web.Mvc/Views/Shared/Components/RightSideBar/RightSideBarViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using poolprobase.Configuration.Ui;
-
-namespace poolprobase.Web.Views.Shared.Components.RightSideBar
-{
-    public class RightSideBarViewModel
-    {
-        public UiThemeInfo CurrentTheme { get; set; }
-    }
-}
